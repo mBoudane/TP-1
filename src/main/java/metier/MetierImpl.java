43d63778ec596b0a710df5d188321b378ab9c71d
mBoudane <mohamedboudane6@gmail.com>
2024-03-14 22:46:09 +0000

    2nd commit

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,19 +1,14 @@
 package metier;
-
 import dao.IDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
 @Component
 public class MetierImpl implements IMetier{
-
     private IDao dao; // Couplage faible pour l'indépandence de l'interface IDao //
-
     public MetierImpl(IDao dao) {
         this.dao = dao;
     }
-
     @Override
     public double calcul() {
         double data = dao.getData();
@@ -21,8 +16,7 @@ public class MetierImpl implements IMetier{
         return res;
     }
     /*
-        -> Pour permettre d'injecter dans la valeur dao
-        -> Un objet d'une classe qui implémente l'interface IDao
+        -> Pour permettre d'injecter dans la valeur dao / -> Un objet d'une classe qui implémente l'interface IDao
      */
     public void setDao(IDao dao)
     {
